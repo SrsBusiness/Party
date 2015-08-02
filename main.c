c636115a548f4ffc847d64ea359dc7f48801921c
Danny Chen <dannychen579@utexas.edu>
2015-08-02 03:03:45 -0500

    Added src file containing main

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bitboard.h"
+#include "search.h"
+#include "minicurses.h"
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <depth>\n", argv[0]);
+        return 1;
+    }
+    
+    int depth = atoi(argv[1]);
+
+    board_state pos = initial_state;
+
+    #ifdef DEBUG
+    clear_line();
+    #endif
+    
+    int eval = dumb_search(&pos, -100, 100, depth);
+    printf("%d\n", eval);
+}
