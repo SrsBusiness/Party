cc0e204c0cd1199616de4ee0e04e34830b93ff4b
Danny Chen <dannychen579@utexas.edu>
2015-10-06 13:07:17 -0500

    Started tests for hashing

---
+++ tests/unit/hash_test.h
@@ -0,0 +1,14 @@
+#include "hash.h"
+
+void ZOBRIST_incremental_update_test(void **state) {
+    struct board_state pos = BOARD_INITIAL_STATE;
+    struct move m = {
+        PAWN, NO_PIECE, NO_PIECE,
+        BITBOARD_E2 | BITBOARD_E4, 0ul, 0ul,
+        {{1, 1}, {1, 1}, {0, 0}}
+    };
+    uint64_t expected_hash;
+    make(&pos, &m);
+    expected_hash = ZOBRIST_hash(&pos);
+    assert_int_equal(expected_hash, pos.hash);
+}
